82f008affec7c836610f32a20d7a1f8332206461
RashedAbdullah <maarashed40@gmail.com>
2024-03-26 05:13:10 +0600

    skills

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
     <main>
       <Header />
       <div className="flex flex-col items-center justify-center">
-        <div className="lg:w-[60vw] text-xs lg:text-sm m-5">
+        <div className="lg:w-[60vw] text-xs lg:text-sm m-5 flex flex-col gap-10">
           <About />
           <Skills />
         </div>
--- components/skills.jsx
+++ components/skills.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Title from "./title";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <div className="bg-white shadow p-5">
+      <Title>Skills</Title>
+    </div>
+  );
 };
 
 export default Skills;
