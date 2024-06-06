dd215aa735043c8000e628e6ec027a287e2ef5bc
RashedAbdullah <maarashed40@gmail.com>
2024-06-06 17:20:30 +0600

    More projects

--- data/projects.js
+++ data/projects.js
@@ -1,5 +1,9 @@
 export const projects = [
   {
+    name: "quran-bangla",
+    url: "https://github.com/RashedAbdullah/quran-bangla",
+  },
+  {
     name: "rashed-abdullah",
     url: "https://github.com/RashedAbdullah/rashed-abdullah",
   },
@@ -14,5 +18,17 @@ export const projects = [
   {
     name: "real-time-weather",
     url: "https://github.com/RashedAbdullah/real-time-weather",
+  },
+  {
+    name: "photo-feed",
+    url: "https://github.com/RashedAbdullah/photo-feed",
+  },
+  {
+    name: "learn-arabic",
+    url: "https://github.com/RashedAbdullah/learn-arabic",
+  },
+  {
+    name: "Halal-jibika",
+    url: "https://github.com/RashedAbdullah/Halal-jibika",
   },
 ];
